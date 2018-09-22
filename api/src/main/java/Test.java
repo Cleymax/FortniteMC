3864c9939dbfe3faee4bce1fea1a722b99d905dc
Cleymax (Clément) <clemperrin1@gmail.com>
2018-09-22 11:35:18 +0200

    Gradle modules

---
+++ api/src/main/java/Test.java
@@ -0,0 +1,27 @@
+/**
+ * This file is a part of api.
+ * <p>
+ * Copyright (c) api <> - All rights reserved
+ *
+ * @author Clément Perrin (Cleymax) {@literal <cleymaxpro@gmail.com>}
+ * Created the 22/09/2018 at 11:32.
+ */
+
+public class Test {
+
+    private String name;
+    private int id;
+
+    public Test(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ core/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * This file is a part of api.
+ * <p>
+ * Copyright (c) api <> - All rights reserved
+ *
+ * @author Clément Perrin (Cleymax) {@literal <cleymaxpro@gmail.com>}
+ * Created the 22/09/2018 at 11:32.
+ */
+
+public class Main {
+
+    private Test d = new Test("nn", 0);
+
+}
